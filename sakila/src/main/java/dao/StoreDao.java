c630ee1922b0b6c742979b3b5f5788b55114274b
GDJ46 <GDJ46@DESKTOP-0IE7HRJ>
2022-04-01 14:46:16 +0900

    Store vo 생성, StoreDao 생성 후 selectStoreList메서드 추가, storeList.jsp 파일 생성

---
+++ sakila/src/main/java/dao/StoreDao.java
@@ -0,0 +1,74 @@
+package dao;
+
+import java.util.*;
+import java.sql.*;
+
+public class StoreDao {
+	
+	public List<Map<String, Object>> selectStoreList() {
+		// ArrayList는 List 인터페이스의 구현체 중 하나이다.
+		// HashMap은 Map 인터페이스의 구현체 중 하나이다.
+		List<Map<String, Object>> list = new ArrayList<>(); // 다형성
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try { // -클래스가 없으면 ClassNotFoundException이 발생하니 예외처리를 한다.
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/sakila", "root", "java1234");
+			
+			String sql = "SELECT"
+					+ "		s1.store_id storeId,"
+					+ "		s1.manager_staff_id staffId,"
+					+ "		concat(s2.first_name, ' ', s2.last_name) staffName,"
+					+ "		s1.address_id addressId,"
+					+ " 	CONCAT(a.address, IFNULL(a.address2, ' '), district) staffAddress,"
+					+ "		s1.last_update lastUpdate"
+					+ " FROM store s1"
+					+ " INNER JOIN staff s2"
+					+ " INNER JOIN address a"
+					+ " ON s1.manager_staff_id = s2.staff_id"
+					+ " AND s1.address_id = a.address_id;";
+			stmt = conn.prepareStatement(sql);
+		    rs = stmt.executeQuery();
+		    while(rs.next()) {
+		    	Map<String, Object> map = new HashMap<>(); // 다형성
+		    	map.put("storeId", rs.getInt("storeId"));
+		    	map.put("staffId", rs.getInt("staffId"));
+		    	map.put("staffName", rs.getString("staffName"));
+		    	map.put("addressId", rs.getInt("addressId"));
+		    	map.put("staffAddress", rs.getString("staffAddress"));
+		    	map.put("lastUpdate", rs.getString("lastUpdate"));
+		    	list.add(map);
+		    }
+		} catch (Exception e) { // ClassNotFoundException, SQLException 두 개의 예외를 부모 타입 Exception으로 처리 -> 다형성
+			e.printStackTrace();
+			System.out.println("예외 발생");
+		} finally {
+			// DB자원 해지 - try절에서 예외가 발생하면 자원해지 못한 상태에서 코드가 종료된다. finally절이 필요
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return list;
+	}
+	
+	// selectStoreList() 테스트 코드 <- 단위테스트
+	public static void main(String[] args) {
+		StoreDao dao = new StoreDao();
+		List<Map<String, Object>> list = dao.selectStoreList();
+		for(Map m : list) {
+			System.out.println(m.get("storeId") + ", ");
+			System.out.println(m.get("staffId") + ", ");
+			System.out.println(m.get("staffName") + ", ");
+			System.out.println(m.get("addressId") + ", ");
+			System.out.println(m.get("staffAddress") + ", ");
+			System.out.println(m.get("lastUpdate") + ", ");
+			System.out.println("");
+		}
+	}
+}
--- sakila/src/main/webapp/index.jsp
+++ sakila/src/main/webapp/index.jsp
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>INDEX</title>
 </head>
 <body>
 	<h1>INDEX</h1>
